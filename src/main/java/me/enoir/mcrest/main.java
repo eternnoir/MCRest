1dcf6d384440d591a9d93f664fcbe68287d7a7a3
eternnoir <eternnoir@gmail.com>
2015-03-03 11:11:40 +0800

    Init

---
+++ src/main/java/me/enoir/mcrest/main.java
@@ -0,0 +1,10 @@
+package me.enoir.mcrest;
+
+/**
+ * Created by frank on 2015/3/3.
+ */
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/me/enoir/mcrest/mcRest.java
@@ -0,0 +1,10 @@
+package me.enoir.mcrest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by frank on 2015/3/3.
+ */
+@SpringBootApplication
+public class mcRest {
+}
