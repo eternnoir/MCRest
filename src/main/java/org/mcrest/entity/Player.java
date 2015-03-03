5bd83bdd1f654b6336b6a7c8edb6ace931d2c891
eternnoir <eternnoir@gmail.com>
2015-03-03 17:39:46 +0800

    Add player entity.

--- src/main/java/org/mcrest/entity/Player.java
+++ src/main/java/org/mcrest/entity/Player.java
@@ -6,4 +6,13 @@ import java.io.Serializable;
  * Created by frank on 2015/3/3.
  */
 public class Player implements Serializable {
+    private String displayName;
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
