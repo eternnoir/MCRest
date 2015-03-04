42c967a46e524d15b04dc633494f24e072892315
eternnoir <eternnoir@gmail.com>
2015-03-04 09:33:55 +0800

    Add playerhelper.

---
+++ src/main/java/org/mcrest/utils/PlayerHelper.java
@@ -0,0 +1,10 @@
+package org.mcrest.utils;
+
+import org.mcrest.entity.Player;
+
+/**
+ * Created by frank on 2015/3/4.
+ */
+public class PlayerHelper {
+    public Player[] convertBukkitPlayerToMcRest(Player)
+}
