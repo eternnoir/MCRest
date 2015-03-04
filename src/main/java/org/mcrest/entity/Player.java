ffc38c1b7d9e74adf08a17e0cb078272dec73080
eternnoir <eternnoir@gmail.com>
2015-03-04 09:18:15 +0800

    Add some comment.

--- src/main/java/org/mcrest/entity/Player.java
+++ src/main/java/org/mcrest/entity/Player.java
@@ -3,6 +3,8 @@ package org.mcrest.entity;
 import java.io.Serializable;
 
 /**
+ * Player class is model in minecraft game. It has some attribute for Rest Ues.
+ *
  * Created by frank on 2015/3/3.
  */
 public class Player implements Serializable {
