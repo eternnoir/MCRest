7b172d776d8f9ef5fe2fd04bee528b02e833e0b6
eternnoir <eternnoir@gmail.com>
2015-03-04 09:34:18 +0800

    Add getOnlinePlayers method.

--- src/main/java/org/mcrest/server/BukkitServer.java
+++ src/main/java/org/mcrest/server/BukkitServer.java
@@ -1,13 +1,24 @@
 package org.mcrest.server;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.mcrest.entity.Player;
+
+import java.util.Collection;
 
 /**
+ * The Server bukkit version,
  * Created by frank on 2015/3/3.
  */
 public class BukkitServer implements IServer {
     @Override
     public Player[] getPlayers() {
+        // TODO not imp.
+        return new Player[0];
+    }
+
+    @Override
+    public Player[] getOnlinePlayers() {
+        Collection<? extends org.bukkit.entity.Player> onlinePlayers = Bukkit.getOnlinePlayers();
         return new Player[0];
     }
 }
--- src/main/java/org/mcrest/server/IServer.java
+++ src/main/java/org/mcrest/server/IServer.java
@@ -1,10 +1,11 @@
 package org.mcrest.server;
 
-import org.bukkit.entity.Player;
+import org.mcrest.entity.Player;
 
 /**
  * Created by frank on 2015/3/3.
  */
 public interface IServer {
     public Player[] getPlayers();
+    public Player[] getOnlinePlayers();
 }
