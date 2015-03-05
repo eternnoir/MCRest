6fff42c9cf166ec601c54b9f1aab3c136c08ed92
FrankWang <eternnoir@gmail.com>
2015-03-05 21:39:53 +0800

    Update runTest.sh

--- runTest.sh
+++ runTest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir testDir
+mkdir -p testDir/plugins/PluginMetrics/
 wget -O ./testDir/eula.txt https://raw.githubusercontent.com/eternnoir/docker-spigot/master/eula.txt
 cp ./tmp/spigot-1.8.jar ./testDir
 cp ./build/libs/MCREST-all*.jar ./testDir/plugins
