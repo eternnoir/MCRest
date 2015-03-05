713db3f3f6399f36720eae3e33af2381ae4802a8
eternnoir <eternnoir@gmail.com>
2015-03-05 11:50:14 +0800

    Add PlayerResourece

--- src/main/java/org/mcrest/application/resources/Player/PlayerResourece.java
+++ src/main/java/org/mcrest/application/resources/Player/PlayerResourece.java
@@ -1,7 +1,29 @@
 package org.mcrest.application.resources.Player;
 
+import org.mcrest.ServerManager;
+import org.mcrest.entity.Player;
+import org.restlet.ext.jackson.JacksonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+import java.util.Collection;
+
 /**
  * Created by frank on 2015/3/5.
  */
-public class PlayerResourece {
+public class PlayerResourece extends ServerResource  {
+    private String playerName;
+    private Object player;
+    @Override
+    public void doInit() {
+        this.playerName = getAttribute("name");
+        this.player = null; // Could be a lookup to a domain object.
+    }
+
+    @Get("json")
+    public Representation getRepresentation() {
+        Collection<? extends Player> players = ServerManager.getInstance().getServer().getPlayers();
+        return new JacksonRepresentation<Collection<?extends Player>>(players);
+    }
 }
