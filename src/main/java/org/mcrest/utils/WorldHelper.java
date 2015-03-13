9d112cd611cd7a9d12f9419af7ecec5382aaf4a0
eternnoir <eternnoir@gmail.com>
2015-03-13 10:51:47 +0800

    Add Converter.

--- src/main/java/org/mcrest/utils/WorldHelper.java
+++ src/main/java/org/mcrest/utils/WorldHelper.java
@@ -1,7 +1,20 @@
 package org.mcrest.utils;
 
+import org.mcrest.entity.World;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by frank on 2015/3/13.
  */
 public class WorldHelper {
+    public static Collection<? extends World> ConvertBukkitWorldToRest(Collection<? extends org.bukkit.World>  bWorlds){
+        ArrayList<World> worlds = new ArrayList<World>();
+        for(org.bukkit.World bw : bWorlds){
+            World rw = new World(bw);
+            worlds.add(rw);
+        }
+        return worlds;
+    }
 }
