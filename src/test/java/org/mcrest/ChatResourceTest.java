13da37c1b1213691c6ee468ddc308f9422c8c7a3
eternnoir <eternnoir@gmail.com>
2015-04-03 21:44:45 +0800

    Remove unuse import.

--- src/test/java/org/mcrest/ChatResourceTest.java
+++ src/test/java/org/mcrest/ChatResourceTest.java
@@ -2,7 +2,6 @@ package org.mcrest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
